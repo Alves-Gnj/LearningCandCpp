ab20b35f1ea72c987b7e3195309897649f8ed982
Alves-Gnj <73700024+Alves-Gnj@users.noreply.github.com>
2020-11-21 02:38:13 -0300

    Novo sistema randomico e pontos
    - Agora é usado um sistema que sorteia um número randomico de 1 a 100.
    - Criado um sistema completo para calcular os pontos do jogador.
    - Corrigido bugs referentes ao sistema de pontos e o contador de tentativas que estava indo para números negativos quando era utilizado um input errado.

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
@@ -10,24 +11,28 @@ int main() {
     printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
     printf("******************************************\n");
 
-    int numerosecreto = 42;
+    // Sistema de número secreto random de 1 a 100
+    int segundos = time(0);
+    srand(segundos);
+    int numerogrande = rand();
+    int numerosecreto = numerogrande % 100;
 
     int chute;
     int tentativa = 1;
     int ganhou = 0;
 
+    double pontos = 1000;
+
     while(ganhou == 0){
 
-        printf("***\n");
+        printf("____________\n");
         printf("Tentativa %d.\n", tentativa);     
         printf("Qual eh o seu chute? ");
         scanf("%d", &chute);
-        printf("Seu chute foi %d.\n", chute);
 
         //Autenticação
         if(chute < 0) {
             printf("Nao pode chutar numeros negativos.\n");
-            tentativa--;
             continue;
         }
 
@@ -36,25 +41,32 @@ int main() {
         int menor = (chute < numerosecreto);
 
         if (acertou) {
-            printf("*******\n");
-            printf("Parabens! Voce acertou em %d tentativas!\n", tentativa);
-            printf("Viva sua vida.\n");
+            printf("*************\n");
+            printf("* Parabens! *\n");
+            printf("*************\n\n");
+
+            printf("Voce acertou em %d tentativas!\n", tentativa);
+
+            printf("Viva sua vida.\n\n");
             ganhou = 1;
 
         } else {
-            printf("*******\n");
             printf("Voce errou!\n");
-            printf("Nao desanime, tente outra vez!\n");
-            tentativa++;
 
             if (maior){
                 printf("Seu numero foi MAIOR.\n");
+                tentativa++;
             } else if (menor) {
                 printf("Seu numero foi menor.\n");
+                tentativa++;
             }
         }
+        double pontosPerdidos = abs(chute-numerosecreto)/2.0;     
+        pontos = pontos - pontosPerdidos;
     }
-    printf("***************\n");
-    printf("* FIM DE JOGO *\n");
-    printf("***************\n");
+    
+    printf("=== FIM DE JOGO ===\n");
+    printf("Pontuacao: %.2f.\n", pontos);
+    printf("===================\n\n");
+
 }
