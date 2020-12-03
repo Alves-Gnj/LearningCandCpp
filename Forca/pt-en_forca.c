1da6ed3e8f410ce31a4c118fec45f8ae945e7504
Alves-Gnj <73700024+Alves-Gnj@users.noreply.github.com>
2020-12-03 16:34:39 -0300

    Feature and refactoring
    - Now the player can try up to 5 times
    - Code refactores, some variables moved to Global
    - "int tentativas" renamed to "int chutesDados"

--- Forca/pt-en_forca.c
+++ Forca/pt-en_forca.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+// Variaveis globais
+char palavraSecreta[20];
+char chutes[26];
+int chutesDados = 0;
+
 void abertura()
 {
     printf("*****************\n");
@@ -8,23 +13,21 @@ void abertura()
     printf("*****************\n\n");
 }
 
-void chuta(char chutes[26], int *tentativas)
+void chuta()
 {
     char chute;
     scanf(" %c", &chute);
 
-    chutes[(*tentativas)] = chute;
-    (*tentativas)++;
+    chutes[chutesDados] = chute;
+    chutesDados++;
 }
 
-
-
-int jachutou(char letra, char chutes[26], int tentativas)
+int jaChutou(char letra)
 {
 
     int achou = 0;
 
-    for (int j = 0; j < tentativas; j++)
+    for (int j = 0; j < chutesDados; j++)
     {
         if (chutes[j] == letra)
         {
@@ -35,44 +38,60 @@ int jachutou(char letra, char chutes[26], int tentativas)
     return achou;
 }
 
+void desenhaForca()
+{
+    for (int i = 0; i < strlen(palavraSecreta); i++)
+    {
+        int achou = jaChutou(palavraSecreta[i]);
 
+        if (achou)
+        {
+            printf("%c ", palavraSecreta[i]);
+        }
+        else
+        {
+            printf("_ ");
+        }
+    }
+
+    printf("\n");
+}
+
+void escolhePalavra()
+{
+    sprintf(palavraSecreta, "MELANCIA");
+}
+
+int enforcou()
+{
+    int erros = 0;
+    for (int i = 0; i < chutesDados; i++)
+    {
+        int existe = 0;
+        for (int j = 0; j < strlen(palavraSecreta); j++)
+        {
+            if (chutes[i] == palavraSecreta[j])
+            {
+                existe = 1;
+                break;
+            }
+        }
+        if(!existe) erros++;
+    }
+    return erros >=5;
+}
 
 int main()
 {
-
-    char palavraSecreta[20];
-    sprintf(palavraSecreta, "MELANCIA");
-
     int acertou = 0;
-    int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
-
+    escolhePalavra();
     abertura();
 
     do
     {
-        //imprime a palavra secreta
-        for (int i = 0; i < strlen(palavraSecreta); i++)
-        {
+        desenhaForca();
+        chuta();
 
-            int achou = jachutou(palavraSecreta[i], chutes, tentativas);
-
-            if (achou)
-            {
-                printf("%c ", palavraSecreta[i]);
-            }
-            else
-            {
-
-                printf("_ ");
-            }
-        }
-
-        printf("\n");
-
-        chuta(chutes, &tentativas);
-
-    } while (!acertou && !enforcou);
+    } while (!acertou && !enforcou());
 }
