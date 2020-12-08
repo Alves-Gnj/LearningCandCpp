f21159ee2f69eba57af7528773142db6ae85c39d
Alves-Gnj <73700024+Alves-Gnj@users.noreply.github.com>
2020-12-08 17:25:50 -0300

    Refactor

--- Pacman/pt-eng_pacman.c
+++ Pacman/pt-eng_pacman.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pacman.h" 
+
 char **mapa;
 int linhas;
 int colunas;
+
+void alocaMemoria()
+{
+    mapa = malloc(sizeof(char *) * linhas);
+    for (int i = 0; i < linhas; i++)
+    {
+        mapa[i] = malloc(sizeof(char) * (colunas + 1));
+    }
+}
 
 void liberaMapa()
 {
@@ -13,9 +24,8 @@ void liberaMapa()
     free(mapa);
 }
 
-int main()
+void leMapa()
 {
-
     FILE *f;
     f = fopen("mapa.txt", "r");
     if (f == 0)
@@ -25,25 +35,24 @@ int main()
     }
 
     fscanf(f, "%d %d", &linhas, &colunas);
-    printf("linhas %d colunas %d \n", linhas, colunas);
 
-    // Alocando memória
-    mapa = malloc(sizeof(char *) * linhas);
-    for (int i = 0; i < linhas; i++)
-    {
-        mapa[i] = malloc(sizeof(char) * (colunas + 1));
-    }
-
-    /////////////////
+    alocaMemoria();
 
     for (int i = 0; i < 5; i++)
     {
         fscanf(f, "%s", mapa[i]);
     }
 
+    fclose(f);
+}
+
+int main()
+{
+    leMapa();
+
     for (int i = 0; i < 5; i++)
     {
         printf("%s\n", mapa[i]);
     }
-    fclose(f);
+    liberaMapa();
 }
